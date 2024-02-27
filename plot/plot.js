a30087a3fc1fdbc83c989ec4b0f74f033fd56777
Flightless <cavendish@hnu.edu.cn>
2024-02-27 22:43:34 +0800

    update

--- plot/plot.js
+++ plot/plot.js
@@ -8,12 +8,23 @@ document.querySelectorAll("button").forEach((button) => {
         }
         button.classList.add('active');
         // 获取button内部文本
-        const name = button.innerText;
+        let name = button.innerText;
+        // 变成小写
+        name = name.toLowerCase();
+        console.log(name);
         // 更改图片说明
         document.querySelector("figcaption").innerText = `10 years investment simulation of ${name}`;
-        // 用一个已经定义的异步函数getData来获取数据
-        data = getData(name);
-        // 用echarts绘制图像
-        drawChart(data);
+        // 更改图片链接
+        const url = `https://raw.githubusercontent.com/Pelapis/invest-simulation/4797be2b9d79f8e0133475afaba49515608b5b5b/plots/py/${name}.png`;
+        document.querySelector("img").src = url;
+        // 重新加载图片
+        document.querySelector("img").src = url;
+        // 刷新图片
+        document.querySelector("img").src = url;
+
+        // // 用一个已经定义的异步函数getData来获取数据
+        // data = getData(name);
+        // // 用echarts绘制图像
+        // drawChart(data);
     });
 });
