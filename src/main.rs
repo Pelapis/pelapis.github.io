0a0f469bc6694f13556eb411cfd08f2436de9570
Flightless <pelapi@qq.com>
2024-07-10 16:11:23 +0800

    up

--- src/main.rs
+++ src/main.rs
@@ -12,54 +12,52 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-            head {
-                class: "navbar bg-base-300 justify-center",
-                meta { charset: "UTF-8" }
-                meta {
-                    content: "width=device-width, initial-scale=1.0",
-                    name: "viewport"
-                }
-                title { "主页" }
-                link { href: "daisyui_full.min.css", rel: "stylesheet" }
+        head {
+            meta { charset: "UTF-8" }
+            meta {
+                content: "width=device-width, initial-scale=1.0",
+                name: "viewport"
             }
-            body {
-                header {
-                    h1 { "Investment Simulation Plots" }
-                    p { "CSI300 Index, Maotai and Mengjie" }
-                }
-                aside {
-                    button { "Index" }
-                    button { "Maotai" }
-                    button { "Mengjie" }
-                }
-                main { id: "figures",
-                    figure {
-                        div { class: "plot", "收益对持有期曲线图" }
-                        figcaption { "低水平组🙁（正确率0.45）" }
-                    }
-                    figure {
-                        div { class: "plot", "收益对持有期曲线图" }
-                        figcaption { "中水平组😐（正确率0.5）" }
-                    }
-                    figure {
-                        div { class: "plot", "收益对持有期曲线图" }
-                        figcaption { "高水平组😄（正确率0.55）" }
-                    }
-                }
-                footer {
-                    class: "footer footer-center p-4 bg-base-300 text-base-content",
-                    p {
-                        "\n        Made by "
-                        strong { "Cavendish" }
-                        ". The source code is on\n        "
-                        a { href: "https://github.com/Pelapis/invest-simulation",
-                            "GitHub"
-                        }
-                        ".\n      "
-                    }
-                    a { href: "snake/index.html", "贪吃蛇🐍小游戏" }
-                }
-                script { src: "index.js", r#type: "module" }
+            title { "Investment Simulation - dioxus" }
+            link { href: "daisyui_full.min.css", rel: "stylesheet" }
+        }
+        body { class: "navbar bg-base-300 justify-center",
+            header {
+                h1 { class: "btn btn-ghost text-xl place-self-center", "Investment Simulation Plots" }
             }
+            header { class: "navbar bg-base-300 justify-center",
+                p { "CSI300 Index, Maotai and Mengjie" }
+            }
+            div { role: "tablist", class: "tabs tabs-boxed",
+                button { "Index" }
+                button { "Maotai" }
+                button { "Mengjie" }
+            }
+            main { id: "figures",
+                figure {
+                    div { class: "plot", "收益对持有期曲线图" }
+                    figcaption { "低水平组🙁（正确率0.45）" }
+                }
+                figure {
+                    div { class: "plot", "收益对持有期曲线图" }
+                    figcaption { "中水平组😐（正确率0.5）" }
+                }
+                figure {
+                    div { class: "plot", "收益对持有期曲线图" }
+                    figcaption { "高水平组😄（正确率0.55）" }
+                }
+            }
+            footer { class: "footer footer-center p-4 bg-base-300 text-base-content",
+                p {
+                    "\n        Made by "
+                    strong { "Cavendish" }
+                    ". The source code is on\n        "
+                    a { href: "https://github.com/Pelapis/invest-simulation", "GitHub" }
+                    ".\n      "
+                }
+                a { href: "snake/index.html", "贪吃蛇🐍小游戏" }
+            }
+            script { src: "index.js", r#type: "module" }
+        }
     }
 }
