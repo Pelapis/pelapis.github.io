56ed362e80acd16f0079cfd830ea04129f6598c1
Flightless <pelapi@qq.com>
2024-07-10 22:44:51 +0800

    up

--- src/main.rs
+++ src/main.rs
@@ -12,54 +12,42 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
-        head {
-            meta { charset: "UTF-8" }
-            meta {
-                content: "width=device-width, initial-scale=1.0",
-                name: "viewport"
-            }
-            title { "Investment Simulation - dioxus" }
-            link { href: "daisyui_full.min.css", rel: "stylesheet" }
+        link { href: "daisyui_full.min.css", rel: "stylesheet" }
+        header {
+            h1 { class: "btn btn-ghost text-xl place-self-center", "Investment Simulation Plots" }
         }
-        body { class: "navbar bg-base-300 justify-center",
-            header {
-                h1 { class: "btn btn-ghost text-xl place-self-center", "Investment Simulation Plots" }
+        header { class: "navbar bg-base-300 justify-center",
+            p { "CSI300 Index, Maotai and Mengjie" }
+        }
+        div { role: "tablist", class: "tabs tabs-boxed",
+            button { "Index" }
+            button { "Maotai" }
+            button { "Mengjie" }
+        }
+        main { id: "figures",
+            figure {
+                div { class: "plot", "收益对持有期曲线图" }
+                figcaption { "低水平组🙁（正确率0.45）" }
             }
-            header { class: "navbar bg-base-300 justify-center",
-                p { "CSI300 Index, Maotai and Mengjie" }
+            figure {
+                div { class: "plot", "收益对持有期曲线图" }
+                figcaption { "中水平组😐（正确率0.5）" }
             }
-            div { role: "tablist", class: "tabs tabs-boxed",
-                button { "Index" }
-                button { "Maotai" }
-                button { "Mengjie" }
+            figure {
+                div { class: "plot", "收益对持有期曲线图" }
+                figcaption { "高水平组😄（正确率0.55）" }
             }
-            main { id: "figures",
-                figure {
-                    div { class: "plot", "收益对持有期曲线图" }
-                    figcaption { "低水平组🙁（正确率0.45）" }
+        }
+        footer { class: "footer footer-center p-4 bg-base-300 text-base-content",
+            p {
+                span {
+                    "Made by "
+                    strong { "Cavendish" }
+                    ". The source code is on "
+                    a { href: "https://github.com/Pelapis/invest-simulation", "GitHub." }
                 }
-                figure {
-                    div { class: "plot", "收益对持有期曲线图" }
-                    figcaption { "中水平组😐（正确率0.5）" }
-                }
-                figure {
-                    div { class: "plot", "收益对持有期曲线图" }
-                    figcaption { "高水平组😄（正确率0.55）" }
-                }
             }
-            footer { class: "footer footer-center p-4 bg-base-300 text-base-content",
-                p {
-                    span {
-                        "Made by "
-                        strong { "Cavendish" }
-                        ". The source code is on "
-                        a { href: "https://github.com/Pelapis/invest-simulation",
-                            "GitHub."
-                        }
-                    }
-                }
-                a { href: "snake/index.html", "贪吃蛇🐍小游戏" }
-            }
+            a { href: "snake/index.html", "贪吃蛇🐍小游戏" }
         }
     }
 }
