cc59352db82aabc2a1ea7fe415074e07505f57d4
Flightless <pelapi@qq.com>
2024-10-24 22:19:46 +0800

    add game end and score

--- src/snake.rs
+++ src/snake.rs
@@ -1,6 +1,7 @@
 use std::time::Duration;
 
 use ev::keydown;
+use gloo::dialogs::alert;
 use html::canvas;
 use leptos::*;
 use rand::random;
@@ -14,8 +15,7 @@ const CELL_COUNT: usize = 25;
 
 #[component]
 pub fn Snake() -> impl IntoView {
-    // 创建蛇、食物、按键、方向和世界等状态
-    let pressed_key = create_rw_signal(Directions::None);
+    // 创建蛇、食物、方向和世界等状态
     let snake = create_rw_signal(vec![
         (CELL_COUNT / 2, CELL_COUNT - 3),
         (CELL_COUNT / 2, CELL_COUNT - 2),
@@ -26,6 +26,8 @@ pub fn Snake() -> impl IntoView {
         random::<usize>() % CELL_COUNT,
     ));
     let current_direction = create_rw_signal(Directions::Up);
+
+    // 创建依赖于蛇、食物、方向的世界状态
     let world = move || {
         let mut world = vec![vec![WorldStates::None; CELL_COUNT as usize]; CELL_COUNT as usize];
         for (x, y) in snake.get().iter() {
@@ -35,17 +37,6 @@ pub fn Snake() -> impl IntoView {
         world[x as usize][y as usize] = WorldStates::Food;
         world
     };
-
-    // 创建画布和画笔
-    let canvas = canvas();
-    canvas.set_width(WIDTH);
-    canvas.set_height(HEIGHT);
-    let ctx: CanvasRenderingContext2d = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into()
-        .expect("转换为canvas context2d失败");
 
     // 创建定时器，每隔一段时间移动蛇
     set_interval(
@@ -57,8 +48,16 @@ pub fn Snake() -> impl IntoView {
                 Directions::Down => (head.0, (head.1 + 1) % CELL_COUNT),
                 Directions::Left => ((head.0 + CELL_COUNT - 1) % CELL_COUNT, head.1),
                 Directions::Right => ((head.0 + 1) % CELL_COUNT, head.1),
-                _ => (0, 0),
             };
+
+            // 判断是否碰到蛇身
+            for (x, y) in new_snake.iter() {
+                if new_head == (*x, *y) {
+                    alert(format!("游戏结束！您的得分是：{}！", new_snake.len()).as_str());
+                    window().location().reload().unwrap();
+                }
+            }
+
             new_snake.insert(0, new_head);
             if new_head == food.get() {
                 food.set((
@@ -73,7 +72,18 @@ pub fn Snake() -> impl IntoView {
         Duration::from_millis(100),
     );
 
-    // 渲染画布
+    // 创建画布和画笔
+    let canvas = canvas();
+    canvas.set_width(WIDTH);
+    canvas.set_height(HEIGHT);
+    let ctx: CanvasRenderingContext2d = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into()
+        .expect("转换为canvas context2d失败");
+
+    // 当状态变化时，绘制画布
     create_effect(move |_| {
         let world = world();
 
@@ -97,28 +107,25 @@ pub fn Snake() -> impl IntoView {
                 match world[x][y] {
                     WorldStates::Snake => {
                         ctx.set_fill_style_str("black");
-                        ctx.fill_rect(
-                            x as f64 * WIDTH as f64 / CELL_COUNT as f64,
-                            y as f64 * HEIGHT as f64 / CELL_COUNT as f64,
-                            WIDTH as f64 / CELL_COUNT as f64,
-                            HEIGHT as f64 / CELL_COUNT as f64,
-                        );
                     }
                     WorldStates::Food => {
                         ctx.set_fill_style_str("red");
-                        ctx.fill_rect(
-                            x as f64 * WIDTH as f64 / CELL_COUNT as f64,
-                            y as f64 * HEIGHT as f64 / CELL_COUNT as f64,
-                            WIDTH as f64 / CELL_COUNT as f64,
-                            HEIGHT as f64 / CELL_COUNT as f64,
-                        );
                     }
-                    _ => {}
+                    WorldStates::None => {
+                        continue;
+                    }
                 }
+                ctx.fill_rect(
+                    x as f64 * WIDTH as f64 / CELL_COUNT as f64,
+                    y as f64 * HEIGHT as f64 / CELL_COUNT as f64,
+                    WIDTH as f64 / CELL_COUNT as f64,
+                    HEIGHT as f64 / CELL_COUNT as f64,
+                );
             }
         }
     });
 
+    // 监听键盘事件，改变方向
     window_event_listener(keydown, move |event| {
         let key = event.key();
         let direction = match key.as_str() {
@@ -138,11 +145,12 @@ pub fn Snake() -> impl IntoView {
         <header>
             <h1>"贪吃蛇"</h1>
         </header>
-        <h1> { move || format!("{:?}", pressed_key.get()) } </h1>
+        <h3>"得分：" {move || snake.get().len()}</h3>
         <main
             on: click=move |event| {
                 let click_x = event.offset_x() as f64;
                 let click_y = event.offset_y() as f64;
+
                 // 写出对角线方程，判断点击的位置
                 let direction = match (click_y > click_x, click_y > HEIGHT as f64 - click_x) {
                     (false, false) => Directions::Up,
@@ -150,6 +158,7 @@ pub fn Snake() -> impl IntoView {
                     (true, true) => Directions::Down,
                     (false, true) => Directions::Right,
                 };
+
                 if direction != current_direction.get() && direction != current_direction.get().reverse()  {
                     current_direction.set(direction);
                 }
@@ -171,7 +180,6 @@ enum Directions {
     Down,
     Left,
     Right,
-    None,
 }
 
 impl Directions {
@@ -181,7 +189,6 @@ impl Directions {
             Directions::Down => Directions::Up,
             Directions::Left => Directions::Right,
             Directions::Right => Directions::Left,
-            Directions::None => Directions::None,
         }
     }
 }
