24979ca0cfb26ec824cbbebb288532b241306066
Flightless <pelapi@qq.com>
2026-03-30 01:39:37 +0800

    feat(vega): 添加自定义格式函数并调整图表配置
    添加自定义格式函数用于显示x轴标签，并调整图表的高度、宽度和配置参数

--- src/features/invest/components/vega/index.tsx
+++ src/features/invest/components/vega/index.tsx
@@ -3,6 +3,7 @@ import { wrap } from 'comlink'
 import MyWorker from './worker?worker'
 import { useEffect } from 'react'
 import embed from 'vega-embed'
+import * as vega from "vega";
 
 const DAYS: [number, string][] = [
     [1, '1天'],
@@ -15,6 +16,10 @@ const DAYS: [number, string][] = [
     [1250, '5年'],
     [2500, '10年'],
 ]
+const ddict: Record<number, string> = {}
+DAYS.forEach(([val, label]) => {
+    ddict[val] = label
+})
 
 export default function Vega() {
     useEffect(() => {
@@ -24,14 +29,30 @@ export default function Vega() {
             const vals = await Promise.all(DAYS.map(
                 async ([val, _]) => ({ x: val, y: (await workerApi.compute_data(val)).mean })
             ))
+
             const rst = await embed('#hello-worker', {
                 data: {
                     values: vals,
                 },
-                "config": { "view": { "continuousWidth": 300, "continuousHeight": 300 } },
+                "config": { "customFormatTypes": true },
+                "height": 400,
+                "width": 600,
                 "mark": "line",
                 "encoding": {
-                    "x": { "field": "x", "type": "nominal" },
+                    "x": {
+                        "field": "x",
+                        "type": "quantitative",
+                        "scale": { "type": "log" },
+                        "axis": {
+                            // "values": DAYS.map(([_, label]) => label),
+                            "values": DAYS.map(([v, l]) => v * 1),
+                            "ticks": true,
+                            "formatType": "customFormatA",
+                            // "labelExpr": "datum['y'] + '天'"
+                            // "labelExpr": "datum.value + '天'"
+                            // "labelExpr": "datum.label + '天'"
+                        }
+                    },
                     "y": { "field": "y", "type": "quantitative" }
                 },
                 "params": [
@@ -41,6 +62,12 @@ export default function Vega() {
                         "bind": "scales"
                     }
                 ],
+            }, {
+                expressionFunctions: {
+                    "customFormatA": function customFormatA(datum: number) {
+                        return ddict[datum]
+                    }
+                }
             })
             console.log(rst, rst.view)
         })();
